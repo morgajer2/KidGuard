2530674e6dd4e675d45f584960ca205fc92c26a7
Mor Gajer <morgajer2@gmail.com>
2022-06-02 18:30:39 +0300

    started image gallery

--- pages/imageGallery.js
+++ pages/imageGallery.js
@@ -1,6 +1,5 @@
-import { Text, View, TouchableOpacity, ImageBackground, TextInput, KeyboardAvoidingView, ScrollView, Dimensions } from 'react-native';
+import { Text, View, TouchableOpacity, ImageBackground, TextInput, KeyboardAvoidingView, ScrollView, Dimensions, Image, FlatList } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Image } from 'react-native';
 import React, { useState } from 'react';
 
 import { general_color, gray_color, orange_color, styles } from './styles/styleSheet1';
@@ -12,7 +11,10 @@ import * as toxicity from '@tensorflow-models/toxicity';
 
 export const ImageGalleryScreen = ({ route, navigation }) => {
     const { code } = route.params;
-
+    const [images, setImages] = useState([{ id: '1', Image: require('../assets/Images/addB.png') }, { id: '2', Image: require('../assets/Images/addBGray.png') }]);
+    const { width } = Dimensions.get('window');
+    const SPACING = 10;
+    const THUMB_SIZE = 80;
 
     return (
         <View style={styles.container}>
@@ -22,7 +24,20 @@ export const ImageGalleryScreen = ({ route, navigation }) => {
                 style={styles.background}
             />
             <Text style={{ fontWeight: 'bold', fontSize: 25 }}>{code}</Text>
-
+            <View style={{ height: '90%', width: '90%', borderRadius: 10.0, backgroundColor: '#fff', overflow: 'hidden' }}>
+                <Text
+                    style={{
+                        color: 'white',
+                        fontSize: 32,
+                        marginTop: 50,
+                        marginBottom: 25
+                    }}
+                >
+                    Custom Gallery
+                </Text>
+                {/* Carousel View */}
+                {/* Thumbnail component using FlatList */}
+            </View>
 
         </View>
     );
